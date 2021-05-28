e616bd44a82853c5ae17ded797bf67d927622e12
bambwc20 <bambwc20@naver.com>
2021-05-28 21:34:22 +0900

    #2.3 Creating Account (09:41)

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,5 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
+import { authService } from "fbase";
 
-const Auth = () => <span>Auth</span>;
+const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
+
+  const onChange = (e) => {
+    const {
+      target: { name, value },
+    } = e;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      let data;
+      if (newAccount) {
+        data = await authService.createUserWithEmailAndPassword(
+          email,
+          Password
+        );
+      } else {
+        data = await authService.signInWithEmailAndPassword(email, Password);
+      }
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="text"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="password"
+          required
+          value={Password}
+          onChange={onChange}
+        />
+        <input
+          type="submit"
+          value="Log In"
+          value={newAccount ? "Create Account" : "Log In"}
+        />
+      </form>
+      <div>
+        <button>Continue with Google</button>
+        <button>Continue with Github</button>
+      </div>
+    </div>
+  );
+};
 
 export default Auth;
