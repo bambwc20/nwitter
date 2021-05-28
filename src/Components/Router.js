73b89b5c48ec15c1856212e1a32db688458c6ddb
bambwc20 <bambwc20@naver.com>
2021-05-28 23:44:01 +0900

    #2.6 Log Out (06:37)

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,22 +1,38 @@
 import React from "react";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  HashRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
 import Auth from "routes/Auth";
 import Home from "routes/Home";
+import Navigation from "Components/Navigation";
+import Profile from "routes/Profile";
 
+//Navigation이 존재하려면  {isLoggedIn && <Navigation />}  이게 true여야 한다는 것을 의미
 const AppRouter = ({ isLoggedIn }) => {
   return (
     <Router>
+      {isLoggedIn && <Navigation />}
       <Switch>
         {isLoggedIn ? (
           <>
             <Route exact path="/">
               <Home />
             </Route>
+            <Route exact path="/profile">
+              <Profile />
+            </Route>
+            <Redirect from="*" to="/" />
           </>
         ) : (
-          <Route exact path="/">
-            <Auth />
-          </Route>
+          <>
+            <Route exact path="/">
+              <Auth />
+            </Route>
+            <Redirect from="*" to="/" />
+          </>
         )}
       </Switch>
     </Router>
