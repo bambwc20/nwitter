b396c4f8023e12d2155b5d34f4c99ca463b6aff1
bambwc20 <bambwc20@naver.com>
2021-05-29 17:29:09 +0900

    #4.4 Deleting Files (05:47)

--- src/Components/Nweet.js
+++ src/Components/Nweet.js
@@ -1,4 +1,4 @@
-import { dbService } from "fbase";
+import { dbService, storageService } from "fbase";
 import React, { useState } from "react";
 
 const Nweet = ({ nweetObj, isOwner }) => {
@@ -9,6 +9,7 @@ const Nweet = ({ nweetObj, isOwner }) => {
     const ok = window.confirm("Are you sure you want to delete this nweet?");
     if (ok) {
       await dbService.doc(`nweets/${nweetObj.id}`).delete();
+      await storageService.refFromURL(nweetObj.attachmentURL).delete();
     }
   };
 
