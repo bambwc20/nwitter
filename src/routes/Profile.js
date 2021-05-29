cbf82f8b5d6ad85e701489fd075ce69cbc0ec585
bambwc20 <bambwc20@naver.com>
2021-05-29 17:54:42 +0900

    #5.0 Get My Own Nweets (10:33)

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,14 +1,31 @@
-import { authService } from "fbase";
-import React from "react";
+import { authService, dbService } from "fbase";
+import React, { useEffect } from "react";
 import { useHistory } from "react-router";
 
-const Profile = () => {
+const Profile = ({ userObj }) => {
   const history = useHistory(); //Hooks으로도 가능.
   const onLogOutClick = () => {
     authService.signOut();
     history.push("/");
   };
 
+  const getMyNweets = async () => {
+    const nweets = await dbService
+      .collection("nweets")
+      .where("creatorId", "==", userObj.uid)
+      .orderBy("createdAt")
+      .get();
+    //where를 또 할 수 있지만 지금은 그냥 get으로 가져오면 됨.
+    console.log(
+      nweets.docs.map((doc) => {
+        return doc.data();
+      })
+    );
+  };
+
+  useEffect(() => {
+    getMyNweets();
+  }, []);
   return (
     <>
       <button onClick={onLogOutClick}>Log Out</button>
